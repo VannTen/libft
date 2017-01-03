77795d8ba1be399b8825f0b269f6f23896fa5c4f
Max Gautier <mgautier@student.42.fr>
2017-01-03 16:41:42 +0100

    Correct typo on lstsearch

--- f_lstsearch.c
+++ f_lstsearch.c
@@ -6,12 +6,12 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/03 14:53:38 by mgautier          #+#    #+#             */
-/*   Updated: 2017/01/03 15:37:38 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/01/03 16:41:07 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "list.h"
-#include <stldib.h>
+#include <stdlib.h>
 
 /*
 ** Returns first element that returns the integer ref when the function match
