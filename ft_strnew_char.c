e440fea4fcf22822e7a8a13700e9477421c2fc6b
Max Gautier <mgautier@student.42.fr>
2017-01-13 13:19:01 +0100

    Fix bad loop >>> was going out of bounds

--- ft_strnew_char.c
+++ ft_strnew_char.c
@@ -6,13 +6,13 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/12 17:54:41 by mgautier          #+#    #+#             */
-/*   Updated: 2017/01/12 18:03:47 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/01/13 13:06:12 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strnew_char(size_t size, char c)
+char	*ft_strnew_char(const size_t size, const char c)
 {
 	char	*str;
 	size_t	index;
@@ -21,7 +21,7 @@ char	*ft_strnew_char(size_t size, char c)
 	str = (char*)malloc(sizeof(char) * (size + 1));
 	if (str != NULL)
 	{
-		while (index <= size)
+		while (index < size)
 		{
 			str[index] = c;
 			index++;
