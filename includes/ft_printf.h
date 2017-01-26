4b62a76b063b64d92dadbdbd29bae2c3d0a6944a
Max Gautier <mgautier@student.42.fr>
2017-01-26 15:06:37 +0100

    Add format string and arg_req struct
    While parsing the format string, it will be necessary to keep a
    track of every required argument (by the conversion specifications)
    (and perhaps as well of the number of time it is required)

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/25 15:58:40 by mgautier          #+#    #+#             */
-/*   Updated: 2017/01/25 18:31:33 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/01/26 15:00:50 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,23 @@
 # define FT_PRINTF_H
 
 # include "bool.h"
+# include "list.h"
+# include <stdarg.h>
+
+typedef	struct	s_req_arg
+{
+	unsigned int	arg_index;
+	t_bool			required;
+	void			*data;
+}				t_req_arg;
+
+typedef struct	s_format_string
+{
+	t_lst		*required_arguments;
+	void		*arguments;
+	t_bool		positional_args;
+	va_list		*arg_list;
+}				t_format_string;
 
 struct			s_flags
 {
@@ -25,15 +42,17 @@ struct			s_flags
 	t_bool	thousands_grouping;
 };
 
-typedef struct	s_conversion
+typedef struct	s_conversion t_conversion;
+struct	s_conversion
 {
 	struct s_flags		flags;
 	unsigned int		arg_index;
 	int					field_width;
 	int					precision;
 	short				length_modifier;
-	int					(*convert_count)(struct conversion *self);
-	char				*(*convert)(struct conversion *self);
-}				t_conversion;
+	int					(*convert_count)(t_conversion *self);
+	char				*(*convert)(t_conversion *self);
+	t_format_string		*format_string;
+};
 
 #endif
