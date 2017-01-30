db90c646433646c703a00ce456e4467efe7f51d8
Max Gautier <mgautier@student.42.fr>
2017-01-30 11:33:55 +0100

    Add handling of positional arguments
    Two functions which check for a valid postional arguments
    and pass the string to an atoi (which remains to be written)
    They write to the conversion struct and returns the length of
    argument

--- ft_argument.c
+++ ft_argument.c
@@ -6,21 +6,32 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/29 16:04:52 by mgautier          #+#    #+#             */
-/*   Updated: 2017/01/29 17:22:51 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/01/30 11:42:34 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_get_arg_index(char *arg_specifier, t_format_string *format)
+size_t			ft_set_arg_positional(const char *arg_specifier,
+								t_conversion *convers_specs)
 {
-	if (ft_is_positional(*conversion_specifier))
-	{
-		format->use_positionals = TRUE;
-		convers_specs->arg_index = ft_atoi(conversion_specifier);
-	}
+	size_t	param_length;
+
+	param_length = ft_is_positional(arg_specifier);
+	if (param_length != NO_POSITIONAL)
+		convers_specs->arg_index = ft_atoi_strict(arg_specifier);
+	return (param_length);
+}
+
+static size_t	ft_is_positionnal(const char *arg_specifier)
+{
+	size_t	index;
+
+	index = 0;
+	while (arg_specifier[index] != '\0' && ft_isdigit(arg_specifier[index]))
+		index++;
+	if (arg_specifier[index] == '$')
+		return (index);
 	else
-		format->use_non_positionals = TRUE;
-	if (format->use_positionals && format->use_non_positionals)
-		return (ERR_POSITIONAL_COHERENCE);
+		return (NO_POSITIONAL);
 }
