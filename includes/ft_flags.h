2224b46f37fdf03c04af4d475832d12bf50b799f
Max Gautier <mgautier@student.42.fr>
2017-01-30 18:29:23 +0100

    Add header for ft_flags

---
+++ includes/ft_flags.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_flags.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/30 18:16:22 by mgautier          #+#    #+#             */
+/*   Updated: 2017/01/30 18:26:22 by mgautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_FLAGS_H
+# define FT_FLAGS_H
+
+enum	e_flags
+{
+	ALTERNATE_FORM,
+	ZERO_PADDING,
+	NEGATIVE_FIELD_WIDTH,
+	BLANK,
+	ALWAYS_SIGN,
+	THOUSANDS_SEP,
+	FLAGS_NBR
+};
+
+#endif
