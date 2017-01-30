3ecca6bb22a7f4821c94e21976f4b5f92435571e
Max Gautier <mgautier@student.42.fr>
2017-01-30 12:10:48 +0100

    Remove unnecessary check

--- ft_argument.c
+++ ft_argument.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/29 16:04:52 by mgautier          #+#    #+#             */
-/*   Updated: 2017/01/30 11:42:34 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/01/30 12:10:28 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ size_t			ft_set_arg_positional(const char *arg_specifier,
 	param_length = ft_is_positional(arg_specifier);
 	if (param_length != NO_POSITIONAL)
 		convers_specs->arg_index = ft_atoi_strict(arg_specifier);
-	return (param_length);
+	return (param_length + 1);
 }
 
 static size_t	ft_is_positionnal(const char *arg_specifier)
@@ -28,7 +28,7 @@ static size_t	ft_is_positionnal(const char *arg_specifier)
 	size_t	index;
 
 	index = 0;
-	while (arg_specifier[index] != '\0' && ft_isdigit(arg_specifier[index]))
+	while (ft_isdigit(arg_specifier[index]))
 		index++;
 	if (arg_specifier[index] == '$')
 		return (index);
