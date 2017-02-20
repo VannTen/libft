350544e02dadf04466f6c195a9c69415c15baf1b
Max Gautier <mgautier@student.42.fr>
2017-02-20 16:15:33 +0100

    Add header for argument counter

--- ft_argument_counter.c
+++ ft_argument_counter.c
@@ -1,14 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_argument_counter.c                              :+:      :+:    :+:   */
+/*   ft_var_argument_counter.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/01 14:32:52 by mgautier          #+#    #+#             */
-/*   Updated: 2017/02/20 15:38:20 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/02/20 15:51:12 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ft_types.h"
+#include "ft_variadic_args.h"
+#include "ft_printf.h"
+#include <stdlib.h>
 
 static void	ft_max_arg_required(void *_conversion)
 {
@@ -19,9 +24,9 @@ static void	ft_max_arg_required(void *_conversion)
 		conversion->format_string->arg_count = conversion->arg_index;
 }
 
-t_arg		*ft_argument_counter(t_format_string *format_string)
+t_var_arg		*ft_var_argument_counter(t_format_string *format_string)
 {
-	t_arg	*arg_array;
+	t_var_arg	*arg_array;
 	int		index;
 	int		arg_nbr;
 
@@ -30,13 +35,12 @@ t_arg		*ft_argument_counter(t_format_string *format_string)
 	f_lstiter_content(format_string->conversion_list->begin_lst,
 			&ft_max_arg_required);
 	arg_nbr = format_string->arg_count;
-	arg_array = malloc(sizeof(t_arg) * (arg_nbr + 1));
+	arg_array = malloc(sizeof(t_var_arg) * (arg_nbr + 1));
 	if (arg_array != NULL)
 	{
 		while (index < arg_nbr)
 		{
-			arg_array[index].content = NULL;
-			arg_array[index].type = NO_TYPE;
+			arg_array[index].type = UNKNOWN_TYPE;
 			index++;
 		}
 	}
