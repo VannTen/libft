f5c801df6522efd94b0dbe7023bc6a901b2681f3
Max Gautier <mgautier@student.42.fr>
2017-02-21 15:03:39 +0100

    Add va_list parser (return an array of va_list + type)

--- ft_var_argument_counter.c
+++ ft_var_argument_counter.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_var_argument_counter.c                              :+:      :+:    :+:   */
+/*   ft_var_argument_counter.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/01 14:32:52 by mgautier          #+#    #+#             */
-/*   Updated: 2017/02/21 12:32:16 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/02/21 15:02:54 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,14 @@
 #include "ft_variadic_args.h"
 #include "ft_printf.h"
 #include <stdlib.h>
+#include <stdarg.h>
 
-static void	ft_max_arg_required(void *_conversion)
-{
-	t_conversion	*conversion;
-
-	conversion = _conversion;
-	if (conversion->arg_index > conversion->format_string->arg_count)
-		conversion->format_string->arg_count = conversion->arg_index;
-}
-
-t_var_arg		*ft_arg_list_ctor(t_format_string *format_string)
+static t_var_arg	*ft_arg_list_ctor(size_t arg_nbr)
 {
 	t_var_arg	*arg_array;
-	int		index;
-	int		arg_nbr;
+	size_t		index;
 
-	format_string->arg_count = 0;
 	index = 0;
-	f_lstiter_content(format_string->conversion_list->begin_lst,
-			&ft_max_arg_required);
-	arg_nbr = format_string->arg_count;
 	arg_array = malloc(sizeof(t_var_arg) * (arg_nbr + 1));
 	if (arg_array != NULL)
 	{
@@ -47,7 +34,8 @@ t_var_arg		*ft_arg_list_ctor(t_format_string *format_string)
 	return (arg_array);
 }
 
-void	ft_fill_args_array(t_var_arg *arg_list, va_list *var_args, size_t size)
+static void			ft_fill_args_array(t_var_arg *arg_list, va_list *var_args,
+		size_t size)
 {
 	size_t	index;
 
@@ -58,4 +46,23 @@ void	ft_fill_args_array(t_var_arg *arg_list, va_list *var_args, size_t size)
 		g_advance_by[arg_list[index].type](var_args);
 		index++;
 	}
+}
+
+static unsigned int	ft_max_arg_required(const void *conversion)
+{
+	return (((const t_conversion*)conversion)->arg_index);
+}
+
+t_var_arg			*ft_get_var_args(t_fifo *conv_list, va_list *var_arg_list)
+{
+	t_var_arg			*arg_array;
+	unsigned int		index;
+	unsigned int		arg_nbr;
+
+	index = 0;
+	arg_nbr = f_fifomax(conv_list, &ft_max_arg_required);
+	arg_array = ft_arg_list_ctor(arg_nbr);
+	if (arg_array != NULL)
+		ft_fill_args_array(arg_array, var_arg_list, arg_nbr);
+	return (arg_array);
 }
