e725182d9a5e4d98a685107489827ab1f6559823
Max Gautier <mgautier@student.42.fr>
2017-02-21 16:32:25 +0100

    Join files

--- ft_format_string_args.c
+++ ft_format_string_args.c
@@ -6,17 +6,16 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/07 12:28:05 by mgautier          #+#    #+#             */
-/*   Updated: 2017/02/08 18:13:44 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/02/21 16:01:27 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include "ft_conversion_specifier.h"
 #include "ft_variadic_args.h"
 #include <stdlib.h>
 #include <stdarg.h>
 
-t_type ft_conv_to_type(t_var_arg *variadic, t_conversion *conversion)
+static t_type ft_conv_to_type(t_var_arg *variadic, t_conversion *conversion)
 {
 	if (conversion->type == D || conversion->type == I)
 		variadic[conversion->arg_index].type =
@@ -30,44 +29,6 @@ t_type ft_conv_to_type(t_var_arg *variadic, t_conversion *conversion)
 	else
 		variadic[conversion->arg_index].type = UNKNOWN_TYPE;
 	return (variadic[conversion->arg_index].type);
-}
-
-void		*args_array_ctor(size_t args_number)
-{
-	size_t		index;
-	t_var_arg	*args_array;
-
-	args_array = malloc(sizeof(t_var_arg) * args_number + 1);
-	if (args_array != NULL)
-		return (NULL);
-	index = 0;
-	while (index < args_number)
-	{
-		args_array[index].type = UNKNOWN_TYPE;
-		index++;
-	}
-}
-
-unsigned int	get_arg_index(const void *_conversion)
-{
-	const t_conversion	*conversion;
-
-	conversion = _conversion;
-	return (conversion->arg_index);
-}
-
-static void	ft_get_args(t_var_arg *args_array, size_t arg_count,
-		va_list arg_list)
-{
-	size_t	index;
-
-	index = 0;
-	while (index < arg_count)
-	{
-		va_copy(args_array[index].arg, arg_list);
-		advance_variadic_list[args_array[index].type](&arg_list);
-		index++;
-	}
 }
 
 static void	ft_set_types(t_var_arg *args_array, t_lst *conversion_list,
@@ -94,15 +55,52 @@ static void	ft_set_types(t_var_arg *args_array, t_lst *conversion_list,
 		}
 	}
 }
-
-void		*ft_format_string_args(t_lst *conversion_list, va_list list)
+static t_var_arg	*ft_arg_list_ctor(size_t arg_nbr)
 {
-	size_t	args_number;
-	t_var_arg	*args_array;
+	t_var_arg	*arg_array;
+	size_t		index;
 
-	args_number = f_lstmax(conversion_list, &get_arg_index);
-	args_array = args_array_ctor(args_number);
-	ft_set_types(args_array, conversion_list, args_number);
-	ft_get_args(args_array, args_number, list);
-	return (args_array);
+	index = 0;
+	arg_array = malloc(sizeof(t_var_arg) * (arg_nbr + 1));
+	if (arg_array != NULL)
+	{
+		while (index < arg_nbr)
+		{
+			arg_array[index].type = UNKNOWN_TYPE;
+			index++;
+		}
+	}
+	return (arg_array);
+}
+
+static void			ft_fill_args_array(t_var_arg *arg_list, va_list *var_args,
+		size_t size)
+{
+	size_t	index;
+
+	index = 0;
+	while (index < size)
+	{
+		va_copy(arg_list[index].arg, *var_args);
+		g_advance_by[arg_list[index].type](var_args);
+		index++;
+	}
+}
+
+static unsigned int	ft_max_arg_required(const void *conversion)
+{
+	return (((const t_conversion*)conversion)->arg_index);
+}
+
+t_var_arg			*ft_get_var_args(t_fifo *conv_list, va_list *var_arg_list)
+{
+	t_var_arg			*arg_array;
+	unsigned int		arg_nbr;
+
+	arg_nbr = f_fifomax(conv_list, &ft_max_arg_required);
+	arg_array = ft_arg_list_ctor(arg_nbr);
+	if (arg_array != NULL)
+		ft_set_types(arg_array, conv_list->begin_lst, arg_nbr);
+		ft_fill_args_array(arg_array, var_arg_list, arg_nbr);
+	return (arg_array);
 }
