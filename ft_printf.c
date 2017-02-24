04c8802b673803de8aabe52911e39856eae3ced8
Max Gautier <mgautier@student.42.fr>
2017-02-24 18:49:02 +0100

    Add printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,22 +6,25 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/21 15:12:26 by mgautier          #+#    #+#             */
-/*   Updated: 2017/02/24 10:36:20 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/02/24 18:48:43 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "format_string_defs.h"
-#include "variadic_args_interface.h"
+#include "variadic_args_defs.h"
 #include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
 
 int	ft_vprintf(const char *format_string, va_list *var_arg_list)
 {
 	t_format_string	*fmt;
 	int				written;
 
+	written = 0;
 	fmt = ft_format_string_parser(format_string);
 	fmt->arg_list = ft_get_var_args(fmt->conversion_list, var_arg_list);
-	written = 0;
+//	written = ft_get_resulting_length(fmt);
 	return (written);
 }
 
