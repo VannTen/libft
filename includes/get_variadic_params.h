395e1698ad68288cfeb9ef79165fb5d7613d6c87
Max Gautier <mgautier@student.42.fr>
2017-03-20 18:51:42 +0100

    More define as intitializers

--- includes/get_variadic_params.h
+++ includes/get_variadic_params.h
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/07 12:48:04 by mgautier          #+#    #+#             */
-/*   Updated: 2017/03/20 11:53:56 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/03/20 17:30:57 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,36 +39,42 @@ void		get_long_long_ptr_param(t_var_arg *param, va_list *arg_list);
 void		get_intmax_t_ptr_param(t_var_arg *param, va_list *arg_list);
 void		get_ptrdiff_t_ptr_param(t_var_arg *param, va_list *arg_list);
 void		get_size_t_ptr_param(t_var_arg *param, va_list *arg_list);
+void		get_wint_param(t_var_arg *param, va_list *arg_list);
+void		get_wstring_param(t_var_arg *param, va_list *arg_list);
 typedef void	(*t_get_variadic)(t_var_arg *param, va_list *);
 
 # define CONST_GET_ARG_INITIALIZER\
 	&get_no_param,\
 	&get_int_param,\
-	&get_char_param,\
-	&get_short_param,\
 	&get_long_param,\
 	&get_long_long_param,\
+	&get_char_param,\
+	&get_short_param,\
 	&get_intmax_t_param,\
 	&get_ptrdiff_t_param,\
 	&get_size_t_param,\
 	NULL,\
 	&get_uint_param,\
-	&get_uchar_param,\
-	&get_ushort_param,\
 	&get_ulong_param,\
 	&get_ulong_long_param,\
+	&get_uchar_param,\
+	&get_ushort_param,\
 	&get_uintmax_t_param,\
 	&get_ptrdiff_t_param,\
 	&get_size_t_param,\
 	NULL,\
 	&get_int_ptr_param,\
-	&get_char_ptr_param,\
-	&get_short_ptr_param,\
 	&get_long_ptr_param,\
 	&get_long_long_ptr_param,\
+	&get_char_ptr_param,\
+	&get_short_ptr_param,\
 	&get_intmax_t_ptr_param,\
 	&get_ptrdiff_t_ptr_param,\
 	&get_size_t_ptr_param,\
-	NULL
+	NULL,\
+	&get_char_param,\
+	&get_wint_param,\
+	&get_char_ptr_param,\
+	&get_wstring_param
 
 #endif
--- includes/length_modifier_defs.h
+++ includes/length_modifier_defs.h
@@ -6,20 +6,21 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/30 19:03:56 by mgautier          #+#    #+#             */
-/*   Updated: 2017/03/10 16:06:48 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/03/20 16:57:23 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LENGTH_MODIFIER_DEFS_H
 # define LENGTH_MODIFIER_DEFS_H
+# define CONST_LENGTH_MODIF_INTIALIZER "\0llhhjtzq"
 
 typedef enum	e_length_modifier
 {
 	NONE,
-	VERY_SHORT,
-	SHORT,
 	LONG,
 	VERY_LONG,
+	SHORT,
+	VERY_SHORT,
 	MAX_INT,
 	PTR_DIFF,
 	SIZE,
