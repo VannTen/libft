0d3e13b35912c9ee09862cb6d345634ff17fb945
Max Gautier <mgautier@student.42.fr>
2017-03-20 10:35:05 +0100

    Normalize ft_wctomb, add header files

--- ft_wctomb.c
+++ ft_wctomb.c
@@ -6,11 +6,12 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/17 15:04:41 by mgautier          #+#    #+#             */
-/*   Updated: 2017/03/17 19:10:02 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/03/20 10:34:42 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <wchar.h>
+#include "wide_char_defs.h"
 
 int	ft_wctomb_len(wchar_t wchar)
 {
@@ -29,27 +30,27 @@ int	ft_wctomb_len(wchar_t wchar)
 	return (size_of_mb_seq);
 }
 
-#define CONTINUATION_BYTE (unsigned int)0200
-int	fta_wctomb(char *dst, wchar_t wchar, int size_mb_seq)
+int	ft_wctomb_write(char *dst, wchar_t wchar, int size_mb_seq)
 {
-	int			index;
-	int			shift;
+	int					index;
+	int					shift;
 	const unsigned int	mask[] = {07, 017, 037, 0};
 	const unsigned int	utf_8_len_indic[] = {0360, 0340, 0300, 0};
 
 	index = 4 - size_mb_seq;
+	if (index < 0)
+		return (index);
 	shift = 18 - 6 * index;
 	dst = dst - index;
 	dst[index] = (unsigned char)((wchar >>shift) & mask[index]);
 	dst[index] += utf_8_len_indic[index];
 	index++;
-	shift -= 6;
 	while (index < 4)
 	{
+		shift -= 6;
 		dst[index] = (unsigned char)((wchar >>shift) & (unsigned int)077);
 		dst[index] += CONTINUATION_BYTE;
 		index++;
-		shift -= 6;
 	}
 	return (size_mb_seq);
 }
@@ -59,5 +60,5 @@ int ft_wctomb(char *dst, wchar_t wchar)
 	int size;
 
 	size = ft_wctomb_len(wchar);
-	return (fta_wctomb(dst, wchar, size));
+	return (ft_wctomb_write(dst, wchar, size));
 }
---
+++ includes/wide_char_interface.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wide_char_interface.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/20 10:29:40 by mgautier          #+#    #+#             */
+/*   Updated: 2017/03/20 10:33:15 by mgautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WIDE_CHAR_INTERFACE_H
+# define WIDE_CHAR_INTERFACE_H
+# include <wchar.h>
+
+int	ft_wctomb_len(wchar_t wchar);
+int	ft_wctomb_write(char *dst, wchar_t wchar, int size_mb_seq);
+int ft_wctomb(char *dst, wchar_t wchar);
+
+#endif
