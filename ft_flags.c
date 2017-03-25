e67cbea5f0278a09aa21e4e335340c4515685270
Max Gautier <mgautier@student.42.fr>
2017-03-25 11:19:57 +0100

    Suppress old functions

--- ft_flags.c
+++ ft_flags.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/30 18:17:02 by mgautier          #+#    #+#             */
-/*   Updated: 2017/03/20 13:36:04 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/03/25 11:09:16 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,66 +46,4 @@ int				set_flags(const char *conversion_specifier,
 	while (set_one_flag(conversion_specifier, index, convers_specs))
 		index++;
 	return (index);
-}
-
-static int		ft_write_alternate_form(char *to_write,
-		const t_conversion *conv)
-{
-	int	flags_len;
-
-	flags_len = 0;
-	if (conv->flags[ALTERNATE_FORM])
-	{
-		if ((conv->type == X || conv->type == X_MAJ)
-				&& has_no_null_value(conv->arg))
-		{
-			ft_strcpy(to_write, conv->type == X ?
-					HEXA_ALTERNATE_FORM : HEXA_MAJ_ALTERNATE_FORM);
-			flags_len += ft_strlen(HEXA_ALTERNATE_FORM);
-		}
-		else if (conv->type == O)
-		{
-			ft_strcpy(to_write, OCTAL_ALTERNATE_FORM);
-			flags_len += ft_strlen(OCTAL_ALTERNATE_FORM);
-		}
-	}
-	return (flags_len);
-}
-
-int				ft_write_flags(char *to_write, const t_conversion *conv)
-{
-	int	flags_len;
-
-	flags_len = ft_write_alternate_form(to_write, conv);
-	if (conv->flags[ALWAYS_SIGN] && is_signed_positive(conv->arg))
-	{
-		to_write[flags_len] = '+';
-		flags_len++;
-	}
-	else if (conv->flags[BLANK] && is_signed_positive(conv->arg))
-	{
-		to_write[flags_len] = ' ';
-		flags_len++;
-	}
-	return (flags_len);
-}
-
-int				ft_flags_len(const t_conversion *conv)
-{
-	int	flags_len;
-
-	flags_len = 0;
-	if (conv->flags[ALTERNATE_FORM])
-	{
-		if ((conv->type == X || conv->type == X_MAJ)
-				&& has_no_null_value(conv->arg))
-			flags_len += ft_strlen(HEXA_ALTERNATE_FORM);
-		else if (conv->type == O)
-			flags_len += ft_strlen(OCTAL_ALTERNATE_FORM);
-	}
-	if (conv->flags[ALWAYS_SIGN] && is_signed_positive(conv->arg))
-		flags_len++;
-	else if (conv->flags[BLANK] && is_signed_positive(conv->arg))
-		flags_len++;
-	return (flags_len);
 }
