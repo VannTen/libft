3214bdb844c2821038583ff26c147e1b4c09568e
Max Gautier <mgautier@student.42.fr>
2017-03-27 13:17:35 +0200

    Fix bug : wrong arg demand for int_params

--- ft_conversion_private_is_valid.c
+++ ft_conversion_private_is_valid.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/17 14:31:08 by mgautier          #+#    #+#             */
-/*   Updated: 2017/03/22 12:09:13 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/03/27 12:52:39 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ t_bool	ft_conversion_arg_is_valid(const t_conversion *conv)
 
 t_bool	ft_precision_arg_is_valid(const t_conversion *conv)
 {
-	return (conv->precision.index_is_valid);
+	return (conv->precision.is_arg && conv->precision.index_is_valid);
 }
 
 t_bool	ft_field_width_arg_is_valid(const t_conversion *conv)
 {
-	return (conv->precision.index_is_valid);
+	return (conv->field_width.is_arg && conv->precision.index_is_valid);
 }
 
 t_bool	conversion_result_produces_error(const t_conversion *conv)
