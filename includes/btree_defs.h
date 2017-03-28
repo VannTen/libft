26760f7002c40dbea4a1880835f8237dd6d108b7
Max Gautier <mgautier@student.42.fr>
2017-03-28 16:49:28 +0200

    Add struct for btree

---
+++ includes/btree_defs.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_defs.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/28 16:41:31 by mgautier          #+#    #+#             */
+/*   Updated: 2017/03/28 16:51:28 by mgautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BTREE_DEFS_H
+# define BTREE_DEFS_H
+# include "btree_interface.h"
+
+struct	s_node
+{
+	void			*content;
+	struct s_node	*left;
+	struct s_node	*right;
+};
+
+typedef int	(*t_compare)(void *content);
+struct	s_btree
+{
+	struct s_node	*root;
+	t_compare			f_comp;
+};
+
+#endif
