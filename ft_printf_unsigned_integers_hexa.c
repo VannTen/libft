611bf5b05963407eed62117b7e22064a1c548f2e
Max Gautier <mgautier@student.42.fr>
2017-03-28 10:52:33 +0200

    Suppress x_maj functions (treated by x functions)

--- ft_printf_unsigned_integers_hexa.c
+++ ft_printf_unsigned_integers_hexa.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/14 19:02:34 by mgautier          #+#    #+#             */
-/*   Updated: 2017/03/27 15:37:02 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/03/28 10:53:24 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,27 +35,18 @@ static int	x_writer(char *to_write, const t_conversion *conv)
 	return (conv->result_length);
 }
 
-int		ft_printf_len_x(t_conversion *conv)
+int			ft_printf_len_x(t_conversion *conv)
 {
 	int conversion_result;
 
-	conversion_result = itoa_len_unsigned(ft_var_unsigned_integers(conv->arg), 16);
+	conversion_result =
+		itoa_len_unsigned(ft_var_unsigned_integers(conv->arg), 16);
 	conv->result_length = conversion_result;
 	conv->supp_length = count_alternate_form(conv);
 	return (length_integers(conv, conversion_result));
 }
 
-void	ft_print_to_x(char *to_write, const t_conversion *conv)
-{
-	write_whole_conv(to_write, conv, &write_alt_form, &x_writer);
-}
-
-int		ft_printf_len_x_maj(t_conversion *conv)
-{
-	return (ft_printf_len_x(conv));
-}
-
-void	ft_print_to_x_maj(char *to_write, const t_conversion *conv)
+void		ft_print_to_x(char *to_write, const t_conversion *conv)
 {
 	write_whole_conv(to_write, conv, &write_alt_form, &x_writer);
 }
