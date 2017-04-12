3fe1e8813c4f3eaad5dcd109d2127e3d7264989d
Max Gautier <mgautier@student.42.fr>
2017-04-12 18:02:15 +0200

    Add usual macros

--- includes/usual_macros.h
+++ includes/usual_macros.h
@@ -6,14 +6,15 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/11 16:43:51 by mgautier          #+#    #+#             */
-/*   Updated: 2017/04/11 16:45:24 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/04/12 14:09:33 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef USUAL_MACROS_H
 # define USUAL_MACROS_H
 
-# define MAX(x, y) x > y ? x : y
-# define MIN(x, y) x < y ? x : y
+# define MAX(x, y) (x > y ? x : y)
+# define MIN(x, y) (x < y ? x : y)
+# define ABS(x) (x > 0 ? x : -x)
 
 #endif
