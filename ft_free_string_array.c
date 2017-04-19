c7466c9599e1cb9ae06a2d27461f9f8bcc83588b
Max Gautier <mgautier@student.42.fr>
2017-04-19 17:02:48 +0200

    Make ft_free_string_array NULL proof
    Following the free convention (NULL can safely be passed to a
    destructor)

--- ft_free_string_array.c
+++ ft_free_string_array.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/23 15:48:31 by mgautier          #+#    #+#             */
-/*   Updated: 2016/12/23 16:22:14 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/04/19 17:02:30 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ char		**ft_free_string_array(char ***tab)
 	char	**to_free;
 
 	to_free = *tab;
-	while (*to_free != NULL)
+	if (to_free != NULL)
 	{
-		ft_strdel(to_free);
-		to_free++;
+		while (*to_free != NULL)
+		{
+			ft_strdel(to_free);
+			to_free++;
+		}
+		free(*tab);
+		*tab = NULL;
 	}
-	free(*tab);
-	*tab = NULL;
 	return (*tab);
 }
