25ae4c51b53d0cc47b5e199807f4bcc8f0688d50
Max Gautier <mgautier@student.42.fr>
2017-05-02 14:56:46 +0200

    Add usage and validator functions init

--- ft_unix_usage.c
+++ ft_unix_usage.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/02 10:59:42 by mgautier          #+#    #+#             */
-/*   Updated: 2017/05/02 11:05:55 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/05/02 14:55:53 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,14 @@ t_synopsis	*init_synopsis(const char *simple_opt_syn,
 	syn.options = simple_opt_apply;
 	syn.options_param = param_opt_apply;
 	return (&syn);
+}
+
+void		add_opt_validator(t_synopsis *syn, t_bool (*is_valid)(int))
+{
+	syn->is_valid = is_valid;
+}
+
+void		add_usage(t_synopsis *syn, void (*usage)(void))
+{
+	syn->usage = usage;
 }
