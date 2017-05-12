e4da3fff554315f284bdc8076d44995bc27eee24
Max Gautier <mgautier@student.42.fr>
2017-05-12 18:55:16 +0200

    Check stat_return

--- is_file.c
+++ is_file.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/10 14:20:09 by mgautier          #+#    #+#             */
-/*   Updated: 2017/05/11 15:28:49 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/05/12 18:53:53 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ t_bool	is_dir(const char *path)
 {
 	struct stat	file_infos;
 
-	stat(path, &file_infos);
-	return (S_ISDIR(file_infos.st_mode) != 0);
+	return (stat(path, &file_infos) != -1
+			&& S_ISDIR(file_infos.st_mode) != 0);
 }
