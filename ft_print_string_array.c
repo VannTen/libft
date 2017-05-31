d85cdba7974317f40ed295fa5eba2e6af975377f
Max Gautier <mgautier@student.42.fr>
2017-05-31 11:59:46 +0200

    print_string_arr: no sep after last string.

--- ft_print_string_array.c
+++ ft_print_string_array.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/18 17:54:22 by mgautier          #+#    #+#             */
-/*   Updated: 2017/05/15 16:28:08 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/05/31 12:03:13 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,16 @@ void	ft_print_string_array(char const *const *str_array, char sep)
 	size_t	index;
 
 	index = 0;
-	if (str_array != NULL)
+	if (str_array != NULL && str_array[index] != NULL)
 	{
-		while (str_array[index] != NULL)
+		while (1)
 		{
 			ft_putstr(str_array[index]);
-			ft_putchar(sep);
 			index++;
+			if (str_array[index] != NULL)
+				ft_putchar(sep);
+			else
+				break ;
 		}
 	}
 }
