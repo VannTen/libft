d61bd217812befbb64ba9a94962267c69f3c9adb
Max Gautier <mgautier@student.42.fr>
2017-09-26 19:29:01 +0200

    Add ASSERT macro
    Compile time assertion

--- includes/useful_macros.h
+++ includes/useful_macros.h
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/26 18:01:54 by mgautier          #+#    #+#             */
-/*   Updated: 2017/09/26 18:04:23 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/09/26 19:26:24 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,17 @@
 */
 
 #define ARRAY_LENGTH(ARRAY) (sizeof(ARRAY)/sizeof(ARRAY[0]))
+
+/*
+** Static assertions
+** That is meant to check condition on elements known at compile time (such as
+** static variables and such)
+** It is inspired from the Linux Kernel, and precisely, I found out the concept
+** here :
+** http://www.pixelbeat.org/programming/gcc/static_assert.html
+*/
+
+#define ASSERT(predicate) ((void)sizeof(char[1 - 2*!(predicate)]))
+
 
 #endif
