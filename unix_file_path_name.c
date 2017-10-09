7312df0a6ed9cfba02b81d17aa77db6a6d280497
Max Gautier <mgautier@student.42.fr>
2017-10-09 19:03:02 +0200

    Mistake on get_no_dir_part fixed.

--- unix_file_path_name.c
+++ unix_file_path_name.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/09 15:29:44 by mgautier          #+#    #+#             */
-/*   Updated: 2017/10/09 15:43:01 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/10/09 18:59:47 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ const char	*get_no_dir_part(char const *full_path_file)
 	size = ft_strlen(full_path_file);
 	while (full_path_file[size] != '/' && size != 0)
 		size--;
-	return (full_path_file + size + 1);
+	return (full_path_file + size);
 }
