471e5e0feead5a0f93575113f79dd7305fe29a61
Max Gautier <mgautier@student.42.fr>
2017-10-12 17:39:13 +0200

    Add ft_to_unix_const

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,11 +6,19 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 15:28:43 by mgautier          #+#    #+#             */
-/*   Updated: 2017/10/09 16:05:46 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/10/12 17:39:03 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "convert_defs.h"
+
+char	f_to_unix_const(char c)
+{
+	if (c == '.')
+		return ('_');
+	else
+		return (f_toupper(c));
+}
 
 char	f_toupper(char c)
 {
