6e0eba7939c112ffcfcce8a2a58d53979cbd702b
Max Gautier <mgautier@student.42.fr>
2017-10-19 14:21:49 +0200

    Makefiles update
    Use a single static lib for each directory

--- Rules.mk
+++ Rules.mk
@@ -6,7 +6,7 @@
 #*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        *#
 #*                                                +#+#+#+#+#+   +#+           *#
 #*   Created: 2016/12/13 19:41:31 by mgautier          #+#    #+#             *#
-#*   Updated: 2017/10/17 16:13:24 by mgautier         ###   ########.fr       *#
+#*   Updated: 2017/10/19 11:52:32 by mgautier         ###   ########.fr       *#
 #*                                                                            *#
 #* ************************************************************************** *#
 
@@ -90,19 +90,38 @@ endif
 # Otherwise, use the linker for an executable
 
 ifeq ($(suffix $(TARGET_$(DIR))),.a)
+
 $(TARGET_$(DIR)): RECIPE = $(LINK_STATIC_LIB)
 LIBPATH_INC += $(DIR)
+
 else ifeq ($(suffix $(TARGET_$(DIR))),.so)
+
 $(TARGET_$(DIR)): RECIPE = $(LINK_SHARED_LIB)
 LIBPATH_INC += $(DIR)
 else
+
 $(TARGET_$(DIR)): RECIPE = $(LINK_EXE)
+
+$(INTERMEDIATE_TARGET): $(OBJ_$(DIR))
+	$(LINK_STATIC_LIB)
+
+vpath $(INTERMEDIATE_TARGET) ../grammar_gen$(DIR)
+
+$(info $(INTERMEDIATE_TARGET) $(DIR))
+
+$(info $(TARGET_$(DIR)))
+
+$(TARGET_$(DIR)): -l$(TARGET_$(DIR)) $(ELSE)\
+	$(patsubst lib%,-l$(BUILD_PREFIX)%,$(LIBS_$(DIR)))
+	$(QUIET) $(RECIPE)
+
 endif
 
 # Local rules
 
-$(TARGET_$(DIR)): $(OBJ_$(DIR)) $(ELSE) $(patsubst lib%,-l$(BUILD_PREFIX)%,$(LIBS_$(DIR)))
-	$(QUIET) $(RECIPE)
+#$(TARGET_$(DIR)): $(OBJ_$(DIR)) $(ELSE)\
+#	$(patsubst lib%,-l$(BUILD_PREFIX)%,$(LIBS_$(DIR)))
+#	$(QUIET) $(RECIPE)
 
 $(eval $(STATIC_OBJ_RULE))
 ifneq ($(strip $(TEST_LOCAL_$(DIR))),)
