f49471c1d3468c86b11e10866c7bb5d20f4fcd80
Max Gautier <mgautier@student.42.fr>
2017-11-30 18:05:22 +0100

    Error reporting in fifo_add.
    For malloc hypothetic failure.

--- f_fifo_add_take.c
+++ f_fifo_add_take.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/31 13:12:14 by mgautier          #+#    #+#             */
-/*   Updated: 2017/03/31 13:13:20 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/11/30 18:04:36 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 t_fifo	*f_fifo_add(t_fifo *fifo, const void *content)
 {
+	t_lst	*new_end;
+
 	if (fifo == NULL)
 		return (NULL);
-	fifo->end_lst = f_add_end_lst(fifo->end_lst, content);
-	if (fifo->end_lst == NULL)
+	new_end = f_add_end_lst(fifo->end_lst, content);
+	if (new_end == NULL)
 		return (NULL);
+	else
+		fifo->end_lst = new_end;
 	if (fifo->begin_lst == NULL)
 		fifo->begin_lst = fifo->end_lst;
 	return (fifo);
