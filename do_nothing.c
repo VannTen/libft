ff6e7a5eba6f56d69a4d937495466e67d7aa8b74
Max Gautier <mgautier@student.42.fr>
2017-12-11 17:00:42 +0100

    Dummy destroyer remove reference

--- do_nothing.c
+++ do_nothing.c
@@ -6,9 +6,11 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/15 12:18:08 by mgautier          #+#    #+#             */
-/*   Updated: 2017/05/26 16:44:13 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/12/11 15:44:32 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stddef.h>
 
 void	do_nothing(void *precious_content)
 {
@@ -17,5 +19,5 @@ void	do_nothing(void *precious_content)
 
 void	no_destroy(void **precious_content)
 {
-	(void)precious_content;
+	*precious_content = NULL;
 }
