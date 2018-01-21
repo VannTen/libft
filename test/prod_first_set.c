9d9989a36e3516f45bc81b7eacc39ea93f7a58c0
Max Gautier <mgautier@student.42.fr>
2018-01-21 16:57:59 +0100

    test prod_First_set

--- test/prod_first_set.c
+++ test/prod_first_set.c
@@ -12,14 +12,14 @@
 
 #include "prods_interface.h"
 #include "libft.h"
+#include "test_interface.h"
 
 static char const	*g_str_symbol[] = {
 	"A B C",
 	"SYM SYM_B",
 	"",
 	"SYM SYM_1 SYM_C",
-	"SYM: SYM_2 SYM_3 SYM_4",
-	"|SYM_1 SYM_B",
+	"SYM: SYM_2 SYM_3 SYM_4|SYM_1 SYM_B",
 	"SYM_1: A B C | D E F |",
 	"SYM_B: G |"
 };
@@ -31,13 +31,33 @@ static char const	*g_first_set[] = {
 	"SYM_2 A D G SYM_C"
 };
 
+static t_bool		equ(void const *sym, void const *str_cmp)
+{
+	return (ft_strequ(get_name(sym), str_cmp));
+}
+
+static t_bool		check_prod_first_set(void const *prod,
+		void const *str_first)
+{
+	t_lst	*first_set;
+	t_lst	*first_set_cmp;
+	t_bool	result;
+
+	first_set = create_first_set_prod(prod);
+	first_set_cmp = f_strsplit_lst(str_first, ' ');
+	result = lst_equ(first_set, first_set_cmp, equ);
+	f_lstdel(&first_set, no_destroy);
+	f_lstdel(&first_set_cmp, ft_gen_strdel);
+	return (result);
+}
+
 static t_bool		test_prod_first_set(t_prod **prod, t_symbol **sym, ...)
 {
-	t_lst	*first_sets;
-
-	first_sets = str_to_first_set_lst(g_first_set, ARRAY_LENGTH(g_first_set));
-
-	return (TRUE);
+	(void)compute_first_sets(sym,
+			ARRAY_LENGTH(g_str_symbol) - ARRAY_LENGTH(g_first_set));
+	return (check_sets(
+				prod, g_first_set, ARRAY_LENGTH(g_first_set),
+				check_prod_first_set));
 }
 
 int	main(void)
