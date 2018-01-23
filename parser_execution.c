dd62174169a755f880d1242685012b1cdd7488d7
Max Gautier <mgautier@student.42.fr>
2018-01-23 19:28:01 +0100

    parse execution stack

--- parser_execution.c
+++ parser_execution.c
@@ -17,10 +17,30 @@
 #include <unistd.h>
 #include <assert.h>
 
+static t_bool		consume_token(void const *token_value, t_lst **exec_stack,
+		t_exec const *functions)
+{
+	void				*exec_token;
+	t_exec_construct	*parent;
+
+	if (functions != NULL)
+	{
+		exec_token = functions->create(token_value);
+		if (exec_token != NULL)
+			return (FALSE);
+		parent = get_lst_elem(*exec_stack, 0);
+		assert(parent != NULL);
+		// TODO: check for success
+		give_to_parent(parent, exec_token);
+		one_less_symbol(exec_stack);
+	}
+}
+
 static t_bool		one_symbol_transition(
 		t_lst **parse_stack,
+		t_lst **exec_stack,
 		void const **token,
-		size_t token_id)
+		size_t (*token_id)(void const *token))
 {
 	void const		*sym;
 	t_prod const	*derivation;
@@ -28,14 +48,14 @@ static t_bool		one_symbol_transition(
 	sym = f_lstpop(parse_stack);
 	if (is_terminal(sym))
 	{
-		if (get_token_id(sym) == token_id)
-			*token = NULL;
+		if (get_token_id(sym) == token_id(*token))
+			return (consume_token(token, exec_stack, get_exec_functions(sym)));
 		else
 			return (FALSE);
 	}
 	else
 	{
-		derivation = get_prod_for_token(sym, token_id);
+		derivation = get_prod_for_token(sym, token_id(*token));
 		if (!(derivation != NULL
 					&& add_prod_to_stack(derivation, parse_stack)))
 			return (FALSE);
@@ -45,15 +65,12 @@ static t_bool		one_symbol_transition(
 
 static t_bool	init(
 		t_lst **parse_stack,
-		void const **token,
 		char const *construct,
 		t_grammar const *gram)
 {
 	t_symbol const	*end_of_input;
 	t_symbol const	*initial_symbol;
 
-	*parse_stack = NULL;
-	*token = NULL;
 	initial_symbol = find_sym_by_name(gram, construct);
 	end_of_input = find_sym_by_name(gram, "END_OF_INPUT");
 	assert(initial_symbol != NULL && end_of_input != NULL);
@@ -71,16 +88,21 @@ void		*execute_construct(
 {
 	void const	*token;
 	t_lst		*parse_stack;
+	t_lst		*exec_stack;
 
-	if (init(&parse_stack, &token, construct, parser->grammar))
+	token = NULL;
+	parse_stack = NULL;
+	exec_stack = NULL;
+	if (init(&parse_stack, construct, parser->grammar))
 	{
 		while (parse_stack != NULL)
 		{
 			if (token == NULL)
 				token = get_token(input);
 			if (!one_symbol_transition(&parse_stack,
+						&exec_stack,
 						&token,
-						parser->get_token_id(token)))
+						parser->get_token_id))
 			{
 				ft_dprintf(STDERR_FILENO, "Syntax error\n");
 				f_lstdel(&parse_stack, no_destroy);
@@ -108,4 +130,3 @@ void	print_stack(t_lst *stack)
 {
 	f_lstiter(stack, print_sym);
 }
-
